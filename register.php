3477f45d5bcd7208cb7275cbd02cf2828edc45be
babast736 <56747691+babast736@users.noreply.github.com>
2021-09-24 18:06:21 -0400

    Create register.php

---
+++ register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Hello World</title>
+</head>
+
+<body style="background-color:black; color:white;">
+<h1>Register</h1>
+<form action="register.php" method="post">
+    Username:<br />
+    <input type="text" name="username" value="" />
+    <br /><br />
+    E-Mail:<br />
+    <input type="text" name="email" value="" />
+    <br /><br />
+    Password:<br />
+    <input type="password" name="password" value="" />
+    <br /><br />
+    <input type="submit" value="Register" />
+</form>
+</head>
+</body>
+
+</html>
