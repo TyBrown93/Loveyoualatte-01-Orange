6c207b6b83d742aa2d6b6c9dd3e5abc65ce57466
mollieworth <90734299+mollieworth@users.noreply.github.com>
2021-10-29 20:17:51 -0400

    Update and rename insertProduct.html to insertProduct.php

---
+++ LoveYouALatte/ShoppingCart/insertProduct.php
@@ -0,0 +1,49 @@
+<?php
+include_once 'config.php';
+if(isset($_POST['submit']))
+{   
+     $name = $_POST['name'];
+     $email = $_POST['email'];
+     $mobile = $_POST['mobile'];
+     $sql = "INSERT INTO products (name,desc,price)
+     VALUES ('$name','$desc','$price')";
+     if (mysqli_query($conn, $sql)) {
+        echo "New record has been added successfully !";
+     } else {
+        echo "Error: " . $sql . ":-" . mysqli_error($conn);
+     }
+     mysqli_close($conn);
+}
+?>
+
+<?=template_header('Insert Product')?>
+
+
+
+<div class="wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h2>Add Menu Item Form</h2>
+                </div>
+                <p>Please fill this form to add products to the menu.</p>
+                <form action="index.php?page=insertProduct" method="post">
+                    <div class="form-group">
+                        <label>Product Name</label>
+                        <input type="text" name="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Product Description</label>
+                        <input type="text" name="desc" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Product Price</label>
+                        <input type="number" name="price" class="form-control">
+                    </div>
+                    <input type="submit" class="btn btn-primary" name="submit" value="Submit">
+                </form>
+            </div>
+        </div>       
+    </div>
+</div>
