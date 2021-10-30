58e9341bdb0b26e0bf53f6f52f761b085d302f2e
mollieworth <90734299+mollieworth@users.noreply.github.com>
2021-10-29 20:18:15 -0400

    Update insertProduct.php

--- LoveYouALatte/ShoppingCart/insertProduct.php
+++ LoveYouALatte/ShoppingCart/insertProduct.php
@@ -18,7 +18,17 @@ if(isset($_POST['submit']))
 
 <?=template_header('Insert Product')?>
 
-
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
 
 <div class="wrapper">
     <div class="container-fluid">
