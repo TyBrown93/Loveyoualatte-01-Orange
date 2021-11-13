ca4bcc099c9d0c33ff1560180a44368562413767
sreeyeshkp <90734221+sreeyeshkp@users.noreply.github.com>
2021-11-13 06:28:04 +0530

    Add files via upload
    Creamer options php code

---
+++ Creamer_Options.php
@@ -0,0 +1,8 @@
+<label for="Creamer">Creamer:</label>
+<select name="Creamer" id="creamer">
+	<option value="">choose a creamer</option>
+    <option value="1">Skim</option>
+     <option value="2">2%</option>
+      <option value="3">Whole</option>
+       <option value="4">Soy</option>
+        <option value="5">Almond</option>
